6c9a56d4388112d81755575b37b736ff27bc378d
Udom <udoman@vt.edu>
2023-03-31 14:20:59 -0400

    Connecting backend to forntend

--- client/src/pages/Myrecipe.js
+++ client/src/pages/Myrecipe.js
@@ -1,10 +1,38 @@
 import * as React from "react";
+import { useState, useEffect } from "react";
+import RecipeCard from "../components/RecipeCard";
 
+function MyRecipe() {
+  const [recipes, setRecipes] = useState([]);
 
-function MyRecipe(){
-    return (
-        <h1>MyRecipe</h1>
-    );
+  useEffect(() => {
+    fetch("/test")
+      .then((response) => response.json())
+      .then((data) => {
+        setRecipes(data);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  console.log(recipes);
+
+  return (
+    <div>
+      {typeof recipes.users === "undefined" ? (
+        <p>Loading...</p>
+      ) : (
+        recipes.users.map((user, i) => <p key={i}>{user}</p>)
+      )}
+    </div>
+    // <div>
+    //   <h1>MyRecipe</h1>
+    //   {recipes.length > 0 ? (
+    //     recipes.map((recipe) => <RecipeCard key={recipe._id} rec={recipe} />)
+    //   ) : (
+    //     <p>Loading...</p>
+    //   )}
+    // </div>
+  );
 }
 
 export default MyRecipe;
--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
-const { MongoClient } = require('mongodb');
+const { MongoClient } = require("mongodb");
 const { getRecipes } = require("./recipes.js");
 //const cors = require("cors");
 //require("dotenv").config({ path: "./config.env" });
 const port = 5000;
-const url = process.env.CONN_STR
+const url = process.env.CONN_STR;
 //app.use(cors());
 app.use(express.json());
 //app.use(require("./routes/record"));
@@ -18,7 +18,7 @@ app.use(express.json());
 //   await client.connect();
 //   const db = client.db("recipe-info");
 //   const coll = db.collection("reicpes");
-  
+
 //   const result = await coll.insertOne(ex);
 //     // display the results of your operation
 //     console.log(result.insertedIds);
@@ -30,59 +30,72 @@ app.use(express.json());
 //   await client.close();
 // }
 
-app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
-});
+app.get("/test", (req, res) => {
+  console.log('hitting test');
+  res.json({"users": ["userOne", "userTwo", "userThree"]})
+})
 
-app.get('/filters', (req, res) => {
+app.get("/filters", (req, res) => {
   res.status(200).json({
     filters: [
       "time",
-      { "energy": ["Easy", "Moderate", "Difficult"] },
-      { "mealType": ["Breakfast", "Lunch", "Dinner", "Sweets"] }
-    ]
+      { energy: ["Easy", "Moderate", "Difficult"] },
+      { mealType: ["Breakfast", "Lunch", "Dinner", "Sweets"] },
+    ],
   });
 });
 
-app.get('/dbtest', (req, res) => {
-  getRecipes()
-    .then(recipes => res.json(recipes));
+app.get("/dbtest", (req, res) => {
+  getRecipes().then((recipes) => res.json(recipes));
   //res.status(200).send("please work");
 });
 
-app.get('/getrecipes', (req, res) => {
-  res.status(200).json([{
-    title: 'Snickerdoodles',
-    time: '20mins',
-    energy: 'Moderate',
-    mealType: 'Sweets',
-    utensils: ['Measuring spoons', '2 bowls', 'stove', 'baking sheet'],
-    ingredients: {
-      Snickerdoodles: [
-        '2 ¾ cups all purpose flour',
-        '2tsp cream of tartar',
-        '½ tsp salt',
-        '1tsp baking powder',
-        '1 cup unsalted butter, softened',
-        '2 eggs',
-        '1 tsp vanilla extract'
-      ],
-      'Cinnamon Sugar Coating:': ['⅓ cup sugar', '2 tbsp cinnamon']
-    },
-    steps: [
-      'Preheat Oven to 350℉',
-      'In a large bowl, cream together butter and sugar',
-      'Mix flour, cream of tartar, baking soda, and salt together in another bowl and then slowly combine wet and dry ingredients',
-      'Combine sugar and cinnamon for the cinnamon sugar coating',
-      'Scoop out dough and roll into a ball',
-      'Bake for 8-10 minutes'
-    ],
-    image: { mime: 'image/jpeg', path: '/some/path/to/file' },
-    filters: ['No Protein']
-  }]);
+app.get("/getrecipes", (req, res) => {
+  getRecipes()
+    .then((recipes) => res.json(recipes))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Internal server error");
+    });
 });
 
-app.get('/', (req, res) => {
+// app.get('/getrecipes', (req, res) => {
+//   res.status(200).json([{
+//     title: 'Snickerdoodles',
+//     time: '20mins',
+//     energy: 'Moderate',
+//     mealType: 'Sweets',
+//     utensils: ['Measuring spoons', '2 bowls', 'stove', 'baking sheet'],
+//     ingredients: {
+//       Snickerdoodles: [
+//         '2 ¾ cups all purpose flour',
+//         '2tsp cream of tartar',
+//         '½ tsp salt',
+//         '1tsp baking powder',
+//         '1 cup unsalted butter, softened',
+//         '2 eggs',
+//         '1 tsp vanilla extract'
+//       ],
+//       'Cinnamon Sugar Coating:': ['⅓ cup sugar', '2 tbsp cinnamon']
+//     },
+//     steps: [
+//       'Preheat Oven to 350℉',
+//       'In a large bowl, cream together butter and sugar',
+//       'Mix flour, cream of tartar, baking soda, and salt together in another bowl and then slowly combine wet and dry ingredients',
+//       'Combine sugar and cinnamon for the cinnamon sugar coating',
+//       'Scoop out dough and roll into a ball',
+//       'Bake for 8-10 minutes'
+//     ],
+//     image: { mime: 'image/jpeg', path: '/some/path/to/file' },
+//     filters: ['No Protein']
+//   }]);
+// });
+
+app.get("/", (req, res) => {
   console.log("hello there");
-  res.status(200).send("Welcome to the backend!");
-})
+  res.status(200).json({ message: "Welcome to the backend!" });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
