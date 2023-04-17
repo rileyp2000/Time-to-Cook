de846467700b29d4bf99fc4dd27a8b88a1adab20
rileyp2000 <rileyp@vt.edu>
2023-04-17 10:46:37 -0400

    add ignore and env file setup

---
+++ pi-install.sh
@@ -0,0 +1,26 @@
+sudo apt-get install gnupg
+curl -fsSL https://pgp.mongodb.com/server-4.4.asc | \
+   sudo gpg -o /usr/share/keyrings/mongodb-server-4.4.gpg \
+   --dearmor
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-4.4.gpg ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+
+sudo apt-get update
+sudo apt-get install -y mongodb-org=4.4.8 mongodb-org-server=4.4.8 mongodb-org-shell=4.4.8 mongodb-org-mongos=4.4.8 mongodb-org-tools=4.4.8
+
+echo "mongodb-org hold" | sudo dpkg --set-selections
+echo "mongodb-org-server hold" | sudo dpkg --set-selections
+echo "mongodb-org-shell hold" | sudo dpkg --set-selections
+echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
+echo "mongodb-org-tools hold" | sudo dpkg --set-selections
+
+sudo chown -R mongodb:mongodb /var/lib/mongodb
+sudo chown -R mongodb:mongodb /var/log/mongodb
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+nvm install node
+
+
+sudo systemctl start mongod
+sudo systemctl enable mongod
+
+# HOSTNAME FOR THE SERVICE NEEDS TO BE  "mongodb://127.0.0.1:27017"
--- server/server.js
+++ server/server.js
@@ -2,33 +2,15 @@ const express = require("express");
 const app = express();
 const { MongoClient } = require('mongodb');
 const { getRecipes, toggleFavorite, deleteRecipe, loadSamples, getFilters, addRecipe, editRecipe } = require("./recipes.js");
+const path = require('path');
+
 //const cors = require("cors");
-//require("dotenv").config({ path: "./config.env" });
+require("dotenv").config({ path: "./config.env" });
 const port = 5000;
 const url = process.env.CONN_STR;
 //app.use(cors());
 app.use(express.json());
-//app.use(require("./routes/record"));
-// get driver connection
-//const dbo = require("./db/conn");
-
-//const client = new MongoClient(uri);
-// try {
-//   // Connect to the MongoDB cluster
-//   await client.connect();
-//   const db = client.db("recipe-info");
-//   const coll = db.collection("reicpes");
-
-//   const result = await coll.insertOne(ex);
-//     // display the results of your operation
-//     console.log(result.insertedIds);
-
-// } catch (e) {
-//   console.error(e);
-// } finally {
-//   // Ensures that the client will close when you finish/error
-//   await client.close();
-// }
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
@@ -108,41 +90,6 @@ app.get("/devpreload", (req, res) => {
   //res.status(200).send("please work");
 });
 
-// app.get('/getrecipes', (req, res) => {
-//   res.status(200).json([{
-//     title: 'Snickerdoodles',
-//     _id: '123123131123123',
-//     time: '20mins',
-//     energy: 'Moderate',
-//     mealType: 'Sweets',
-//     utensils: ['Measuring spoons', '2 bowls', 'stove', 'baking sheet'],
-//     ingredients: {
-//       Snickerdoodles: [
-//         '2 ¾ cups all purpose flour',
-//         '2tsp cream of tartar',
-//         '½ tsp salt',
-//         '1tsp baking powder',
-//         '1 cup unsalted butter, softened',
-//         '2 eggs',
-//         '1 tsp vanilla extract'
-//       ],
-//       'Cinnamon Sugar Coating:': ['⅓ cup sugar', '2 tbsp cinnamon']
-//     },
-//     steps: [
-//       'Preheat Oven to 350℉',
-//       'In a large bowl, cream together butter and sugar',
-//       'Mix flour, cream of tartar, baking soda, and salt together in another bowl and then slowly combine wet and dry ingredients',
-//       'Combine sugar and cinnamon for the cinnamon sugar coating',
-//       'Scoop out dough and roll into a ball',
-//       'Bake for 8-10 minutes'
-//     ],
-//     image: { mime: 'image/jpeg', path: '/some/path/to/file' },
-//     filters: ['No Protein'],
-//     favorite: false
-//   }]);
-// });
-
-app.get("/", (req, res) => {
-  //console.log("hello there");
-  res.status(200).send("Welcome to the backend!");
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
