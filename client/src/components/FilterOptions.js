579d6c97dcf8c8a82d61936cf183092ab0830de9
lingho4 <lliebeler4@vt.edu>
2023-04-21 17:51:44 -0400

    Filter bar search query changes
    Filter bar will now generate options for each category.
    When selecting options, the filter bar will construct the query and print it out

--- client/src/components/FilterOptions.js
+++ client/src/components/FilterOptions.js
@@ -1,17 +1,40 @@
 import React, { useState } from 'react';
+import RadioButtons from './RadioButtons';
 
 const Menu = () => {
+  const [query, setQuery] = useState('');
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [selectedOption, setSelectedOption] = useState('');
+  const proteinOptions = ['Beef', 'Pork', 'Turkey', 'Clam', 'Crab', 'Englishman'];
+  const mealOptions = ['Breakfast', 'Lunch', 'Dinner', 'Snack', 'Dessert'];
+  const energyOptions = ['Easy', 'Moderate', 'Difficult'];
+  const [proteinQuery, setProteinQuery] = useState('');
+  const [mealQuery, setMealQuery] = useState('');
+  const [energyQuery, setEnergyQuery] = useState('');
+  
 
   const handleMenuClick = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
-  const handleOptionClick = (option) => {
-    setSelectedOption(option);
-    setIsMenuOpen(false);
-  };
+  const changeProtein = (newQuery) => {
+    setProteinQuery(newQuery);
+    buildQuery(newQuery, mealQuery, energyQuery);
+  }
+
+  const changeMeal = (newQuery) => {
+    setMealQuery(newQuery);
+    buildQuery(proteinQuery, newQuery, energyQuery);
+  }
+
+  const changeEnergy = (newQuery) => {
+    setEnergyQuery(newQuery);
+    buildQuery(proteinQuery, mealQuery, newQuery);
+  }
+
+  const buildQuery = (query1, query2, query3) => {
+    setQuery('getRecipes?protein='+ query1 + '&mealType=' + query2 + '&energy=' + query3);
+  }
 
   return (
     <div className="Filtering Options">
@@ -21,34 +44,13 @@ const Menu = () => {
       </div>
       {isMenuOpen && (
         <div className="menu-options">
-          
-          <select name="Main Protein" id="proteins">
-            <option value="Any Protein">Any Protein</option>
-            <option value="Beef">Beef</option>
-            <option value="Chicken">Chicken</option>
-            <option value="Fish">Fish</option>
-            <option value="Pork">Pork</option>
-          </select>
-
-          <select name="Meal Type" id="meals">
-            <option value="Any Meal Type">Any Meal Type</option>
-            <option value="Breakfast">Breakfast</option>
-            <option value="Lunch">Lunch</option>
-            <option value="Dinner">Dinner</option>
-            <option value="Other">Other</option>
-          </select>
-
-          <select name="Energy" id="energy">
-            <option value="Any Energy Level">Any Energy Level</option>
-            <option value="Easy">Easy</option>
-            <option value="Moderate">Moderate</option>
-            <option value="Difficult">Difficult</option>
-          </select>
-        </div>
-      )}
-      {selectedOption && (
-        <div className="selected-option">
-          You have selected: {selectedOption}
+          <h1>Select a Protein:</h1>
+          <RadioButtons options={proteinOptions} onQueryChange={changeProtein}/>
+          <h1>Select a Meal Type:</h1>
+          <RadioButtons options={mealOptions} onQueryChange={changeMeal}/>
+          <h1>Select an Energy Level:</h1>
+          <RadioButtons options={energyOptions} onQueryChange={changeEnergy}/>
+          <p>query string: {query}</p>
         </div>
       )}
     </div>
